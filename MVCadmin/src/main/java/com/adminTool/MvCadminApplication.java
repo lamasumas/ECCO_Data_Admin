a2ca924ac9fae6eb29a4fb5d22ee6d5f3912f512
root <navesllanes@gmail.com>
2019-06-28 13:00:42 +0100

    Reset the project

---
+++ MVCadmin/src/main/java/com/adminTool/MvCadminApplication.java
@@ -0,0 +1,13 @@
+package com.adminTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvCadminApplication.class, args);
+	}
+
+}
